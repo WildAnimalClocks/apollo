041d268d6e072ae406371cd3c6816436212edc3c
aineniamh <aine.otoole@ed.ac.uk>
2020-10-21 20:55:23 +0100

    peaclock function available with custom logger and full pipeline

--- peaclock/scripts/count_cpgs.smk
+++ peaclock/scripts/count_cpgs.smk
@@ -1,20 +1,52 @@
+import os
+from Bio import SeqIO
+import peaclockfunks as qcfunk
+
+barcodes = config["barcode_str"].split(",")
+config["barcodes"] = barcodes
+
+rule all:
+    input:
+        os.path.join(config["outdir"], "reports","cpg_counts.csv"),
+        os.path.join(config["outdir"], "reports","cpg_wide.csv"),
+        expand(os.path.join(config["outdir"],"gathered_reads","{barcode}.fastq"), barcode=config["barcodes"])
+
+rule gather_demuxed_reads:
+    input:
+    params:
+        barcode_path = os.path.join(config["outdir"],"demultiplexed_reads","{barcode}"),
+        barcode = "{barcode}"
+    output:
+        reads = os.path.join(config["outdir"],"gathered_reads","{barcode}.fastq")
+    run:
+        with open(output.reads, "w") as fw:
+            reads = []
+            for r,d,f in os.walk(params.barcode_path):
+                for fn in f:
+                    if fn.endswith(".fastq") or fn.endswith(".fq"):
+                        for record in SeqIO.parse(os.path.join(r, fn),"fastq"):
+                            if len(record) > config["min_length"] and len(record) < config["max_length"]:
+                                reads.append(record)
+            SeqIO.write(reads,fw,"fastq")
+            print(qcfunk.green(f"Barcode {params.barcode}: ") + f"{len(reads)}")
+
 rule ParaMethR:
     input:
-        refs = config["reference_fasta"],
-        reads = os.path.join(config["output_path"],"demultiplexed_reads","{barcode}.fastq"),
-        cpg = config["cpg_sites"],
-        matrix = config["matrix"],
+        reads = rules.gather_demuxed_reads.output.reads,
+        refs = config["genes"],
+        cpg_sites = config["cpg_sites"],
+        matrix_file = config["matrix_file"],
     params:
         sample = "{barcode}"
     output:
-        counts_long = os.path.join(config["output_path"],"counts","{barcode}.cpg_counts.csv"),
-        counts_wide = os.path.join(config["output_path"],"counts","{barcode}.cpg_wide.csv")
+        counts_long = os.path.join(config["outdir"],"counts","{barcode}.cpg_counts.csv"),
+        counts_wide = os.path.join(config["outdir"],"counts","{barcode}.cpg_wide.csv")
     shell:
         """
         paramether.py \
             --reads {input.reads:q} \
-            --reference {input.refs:q} \
-            --cpg_csv {input.cpg:q} \
+            --reference {input.genes:q} \
+            --cpg_csv {input.cpg_sites:q} \
             --substitution_matrix {input.matrix:q} \
             --sample {params.sample} \
             --report {output.counts_wide:q} \
@@ -23,12 +55,15 @@ rule ParaMethR:
 
 rule gather_reports:
     input:
-        expand(os.path.join(config["output_path"],"counts","{barcode}.cpg_wide.csv"), barcode = config["barcodes"])
+        expand(os.path.join(config["outdir"],"counts","{barcode}.cpg_wide.csv"), barcode = config["barcodes"])
     output:
-        os.path.join(config["output_path"],"reports","cpg_wide.csv")
+        os.path.join(config["outdir"],"reports","cpg_wide.csv")
     run:
+
+        header = qcfunk.make_cpg_header(config['cpg_csv'])
         with open(output[0],"w") as fw:
-            fw.write("sample,gm7_103,gm7_112,gm7_133,gm7_144,gm7_148,gm7_159,gm7_64,gm7_67,gm7_76,gm7_79,hsp4_100,hsp4_105,hsp4_120,hsp4_126,hsp4_131,hsp4_144,hsp4_70,kcns1_100,kcns1_103,kcns1_111,kcns1_116,kcns1_119,kcns1_121,kcns1_125,kcns1_134,kcns1_139,kcns1_144,kcns1_146,kcns1_162,kcns1_165,kcns1_168,kcns1_178,kcns1_30,kcns1_52,kcns1_54,kcns1_57,kcns1_65,kcns1_80,prima1_127,prima1_141,prima1_54\n")
+            fw.write(f"{header}\n")
+            # fw.write("sample,gm7_103,gm7_112,gm7_133,gm7_144,gm7_148,gm7_159,gm7_64,gm7_67,gm7_76,gm7_79,hsp4_100,hsp4_105,hsp4_120,hsp4_126,hsp4_131,hsp4_144,hsp4_70,kcns1_100,kcns1_103,kcns1_111,kcns1_116,kcns1_119,kcns1_121,kcns1_125,kcns1_134,kcns1_139,kcns1_144,kcns1_146,kcns1_162,kcns1_165,kcns1_168,kcns1_178,kcns1_30,kcns1_52,kcns1_54,kcns1_57,kcns1_65,kcns1_80,prima1_127,prima1_141,prima1_54\n")
             for fn in input:
                 with open(str(fn),"r") as f:
                     for l in f:
@@ -37,9 +72,9 @@ rule gather_reports:
 
 rule gather_counts:
     input:
-        expand(os.path.join(config["output_path"],"counts","{barcode}.cpg_counts.csv"), barcode = config["barcodes"])
+        expand(os.path.join(config["outdir"],"counts","{barcode}.cpg_counts.csv"), barcode = config["barcodes"])
     output:
-        os.path.join(config["output_path"],"reports","cpg_counts.csv")
+        os.path.join(config["outdir"],"reports","cpg_counts.csv")
     run:
         with open(output[0],"w") as fw:
             fw.write("sample,cpg_site,total,c,t,a,g,-\n")
@@ -49,3 +84,5 @@ rule gather_counts:
                         fw.write(l.rstrip() + '\n')
 
             # shell("cat temp.txt '{input}' > '{output}' && rm temp.txt")
+
+
---
+++ peaclock/scripts/peaclockfunks.py
@@ -0,0 +1,336 @@
+
+#!/usr/bin/env python3
+
+import os
+import argparse
+import csv 
+import sys
+from Bio import SeqIO
+from datetime import datetime 
+from datetime import date
+import tempfile
+import pkg_resources
+import yaml
+
+END_FORMATTING = '\033[0m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[93m'
+CYAN = '\u001b[36m'
+DIM = '\033[2m'
+
+def get_defaults():
+    default_dict = {
+        "no_temp":False,
+        "demultiplex":False,
+        "path_to_guppy":False,
+        "output_prefix":"peaclock",
+        "species":"apodemus",
+        "barcodes":"native",
+        "configfile":"config.yaml",
+        "allowed_species":["apodemus","mus","phalacrocorax"],
+        "force":True
+        }
+    return default_dict
+
+def add_arg_to_config(key,arg,config):
+    if arg:
+        config[key] = arg
+
+def look_for_config(configfile_arg,cwd, config):
+    configfile = ""
+    if configfile_arg:
+        configfile = os.path.join(cwd,configfile_arg)
+        path_to_file = os.path.abspath(os.path.dirname(input_file))
+        config["path_to_config"] = path_to_file
+
+        if os.path.isfile(configfile):
+            config["configfile"] = configfile
+        else:
+            sys.stderr.write(cyan(f'Error: cannot find configfile at {configfile}\n'))
+            sys.exit(-1)
+    else:
+        configfile = os.path.join(cwd, "config.yaml")
+        config["path_to_config"] = cwd
+        if os.path.isfile(configfile):
+            config["configfile"] = configfile
+        else:
+            configfile = ""
+            print(cyan(f'Note: cannot find configfile at {configfile}'))
+    
+    return configfile
+
+def parse_yaml_file(configfile,config):
+    with open(configfile,"r") as f:
+        input_config = yaml.load(f, Loader=yaml.FullLoader)
+        for key in input_config:
+            snakecase_key = key.replace("-","_").lstrip("-")
+            config[snakecase_key] = input_config[key]
+
+def make_timestamped_outdir(cwd,outdir,config):
+
+    output_prefix = config["output_prefix"]
+    split_prefix = output_prefix.split("_")
+    if split_prefix[-1].startswith("20"):
+        output_prefix = '_'.join(split_prefix[:-1])
+    config["output_prefix"] = output_prefix
+    species = config["species"]
+    timestamp = str(datetime.now().isoformat(timespec='milliseconds')).replace(":","").replace(".","").replace("T","-")
+    outdir = os.path.join(cwd, f"{output_prefix}_{species}_{timestamp}")
+    rel_outdir = os.path.join(".",timestamp)
+
+    return outdir, rel_outdir
+
+def get_outdir(outdir_arg,output_prefix_arg,cwd,config):
+    outdir = ''
+    
+    add_arg_to_config("output_prefix",output_prefix_arg, config)
+
+    if outdir_arg:
+        expanded_path = os.path.expanduser(outdir_arg)
+        outdir = os.path.join(cwd,expanded_path)
+        rel_outdir = os.path.relpath(outdir, cwd) 
+
+    else:
+        outdir, rel_outdir = make_timestamped_outdir(cwd,outdir,config)
+    
+    today = date.today()
+    d = today.strftime("%Y-%m-%d")
+    output_prefix = config["output_prefix"]
+    split_prefix = output_prefix.split("_")
+    if split_prefix[-1].startswith("20"):
+        output_prefix = '_'.join(split_prefix[:-1])
+    config["output_prefix"] = f"{output_prefix}_{d}"
+
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+
+    print(green(f"Output dir:") + f" {outdir}")
+    config["outdir"] = outdir 
+    config["rel_outdir"] = os.path.join(".",rel_outdir) 
+
+def get_temp_dir(tempdir_arg,no_temp_arg, cwd,config):
+    tempdir = ''
+    outdir = config["outdir"]
+    if no_temp_arg:
+        print(green(f"--no-temp:") + f" All intermediate files will be written to {outdir}")
+        tempdir = outdir
+        config["no_temp"] = no_temp_arg
+    elif config["no_temp"]:
+        print(green(f"--no-temp:") + f" All intermediate files will be written to {outdir}")
+        tempdir = outdir
+    elif tempdir_arg:
+        expanded_path = os.path.expanduser(tempdir_arg)
+        to_be_dir = os.path.join(cwd,expanded_path)
+        if not os.path.exists(to_be_dir):
+            os.mkdir(to_be_dir)
+        temporary_directory = tempfile.TemporaryDirectory(suffix=None, prefix=None, dir=to_be_dir)
+        tempdir = temporary_directory.name
+
+    elif "tempdir" in config:
+        expanded_path = os.path.expanduser(config["tempdir"])
+        to_be_dir = os.path.join(cwd,expanded_path)
+        if not os.path.exists(to_be_dir):
+            os.mkdir(to_be_dir)
+        temporary_directory = tempfile.TemporaryDirectory(suffix=None, prefix=None, dir=to_be_dir)
+        tempdir = temporary_directory.name
+
+    else:
+        temporary_directory = tempfile.TemporaryDirectory(suffix=None, prefix=None, dir=None)
+        tempdir = temporary_directory.name
+    
+    config["tempdir"] = tempdir 
+    return tempdir
+    
+def get_read_length_filter(config):
+    lengths = []
+    for record in SeqIO.parse(config["genes"],"fasta"):
+        lengths.append(len(record))
+    lengths = sorted(lengths)
+    config["min_length"] = lengths[0]
+    config["max_length"] = lengths[-1] + 200
+
+def get_package_data(thisdir,species_arg, config):
+    matrix_file = pkg_resources.resource_filename('peaclock', "substitution_matrix.txt")
+    config["matrix_file"] = matrix_file
+
+    add_arg_to_config("species",species_arg, config)
+    
+    species = config["species"]
+
+    if species in config["allowed_species"]:
+        cpg_sites = pkg_resources.resource_filename('peaclock', f"{spceies}/cpg_sites.csv")
+        genes = pkg_resources.resource_filename('peaclock', f"{spceies}/genes.fasta")
+        primer_sequences = pkg_resources.resource_filename('peaclock', f"{spceies}/primer_sequences.csv")
+
+        config["cpg_sites"] = cpg_sites
+        config["genes"] = genes
+        config["primer_sequences"] = primer_sequences
+    else:
+        sys.stderr.write(cyan(f'Error: {species} specified not configured in PEAClock\nPlease select an alternative species\n'))
+        sys.exit(-1)
+
+
+
+def look_for_guppy_barcoder(demultiplex_arg,path_to_guppy_arg,cwd,config):
+    qcfunk.add_arg_to_config(demultiplex_arg, "demultiplex",config)
+    qcfunk.add_arg_to_config(path_to_guppy_arg, "path_to_guppy",config)
+
+    if config["demultiplex"]:
+        if config["path_to_guppy"]:
+            expanded_path = os.path.expanduser(config["path_to_guppy"])
+            path_to_guppy = os.path.join(cwd,expanded_path,"guppy_barcoder")
+            os_cmd = path_to_guppy
+            if os.system(os_cmd) != 0:
+                sys.stderr.write(cyan(f'Error: guppy_barcoder at {path_to_guppy} fails to run\n'))
+                sys.exit(-1)
+        else:
+            sys.stderr.write(cyan(f'Error: please provide the path to guppy_barcoder or run demultiplexing in MinKNOW\n'))
+            sys.exit(-1)
+
+def look_for_basecalled_reads(read_path_arg,cwd,config):
+
+    if read_path_arg:
+        read_path = os.path.join(cwd, read_path_arg)
+        if not os.path.exists(read_path):
+            sys.stderr.write(cyan('Error: cannot find reads at {}\n'.format(read_path)))
+            sys.exit(-1)
+        else:
+            config["read_path"] = read_path
+
+    elif "read_path" in config:
+        if config["read_path"]:
+            expanded_path = os.path.expanduser(config["read_path"])
+            read_path = os.path.join(config["path_to_config"], expanded_path)
+        
+            fq_files = 0
+            for r,d,f in os.walk(read_path):
+                for fn in f:
+                    filename = fn.lower()
+                    if filename.endswith(".fastq") or filename.endswith(".fq"):
+                        fq_files +=1
+
+            if fq_files > 0:
+                print(f"Found {fq_files} fastq files in the input directory")
+            else:
+                sys.stderr.write(cyan('Error: cannot find fastq files at {}\nPlease check your `--read-dir`'.format(read_path)))
+                sys.exit(-1)
+    else:
+        sys.stderr.write(cyan('Error: `--read-dir` needed. Please input the path to the fastq read files either in the config file or via the command line.\n'))
+        sys.exit(-1)
+
+def look_for_barcodes_csv(barcodes_csv_arg,cwd,config):
+    barcodes_csv = ""
+    if barcodes_csv_arg:
+    
+        barcodes_csv = os.path.join(cwd,barcodes_csv_arg)
+        if not os.path.exists(barcodes_csv):
+            sys.stderr.write('Error: cannot find barcodes csv at {}\n'.format(barcodes_csv))
+            sys.exit(-1)
+    elif "barcodes_csv" in config:
+        if config["barcodes_csv"]:
+            expanded_path = os.path.expanduser(config["barcodes_csv"])
+            barcodes_csv = os.path.join(config["path_to_config"], expanded_path)
+        else:
+            config["barcodes_csv"] = ""
+    else:
+        config["barcodes_csv"] = ""
+        sys.stderr.write(cyan('Error: `--barcodes-csv` needed. Please input the path to the barcodes csv either in the config file or via the command line.\n'))
+        sys.exit(-1)
+
+    print(f"Input barcodes csv file: {barcodes_csv}")
+    if barcodes_csv:
+        barcodes = []
+        with open(barcodes_csv, newline="") as f:
+            reader = csv.DictReader(f)
+            column_names = reader.fieldnames
+            if "barcode" not in column_names:
+                sys.stderr.write(f"Error: Barcode file missing header field `barcode`\n")
+                sys.exit(-1)
+            for row in reader: 
+                if row["barcode"].startswith("NB") or row["barcode"].startswith("BC"):
+                    barcodes.append(row["barcode"])
+                else:
+                    sys.stderr.write(f"Error: Please provide barcodes in the format `NB01` or `BC01`\n")
+                    sys.exit(-1)
+                    
+            print(f"{len(barcodes)} barcodes read in from file")
+            for i in barcodes:
+                print(f"  - {i}")
+            barcodes = ",".join(barcodes)
+    else:
+        barcodes_csv = ""
+        print(f"No barcodes csv file input, assuming only one sample.")
+
+def check_barcode_kit():
+
+    add_arg_to_config("barcode_kit",barcode_kit_arg, config)
+    barcode_kit = config["barcode_kit"]
+    if args.barcode_kit.lower() in ["native","pcr","rapid","all"]:
+        config["barcode_set"] = args.barcode_kit.lower()
+    else:
+        sys.stderr.write(f"Error: Please enter a valid barcode kit: one of\n\t-native\n\t-pcr\n\t-rapid\n\t-all\n")
+        sys.exit(-1)
+
+
+def get_snakefile(thisdir):
+    snakefile = os.path.join(thisdir, 'scripts','Snakefile')
+    if not os.path.exists(snakefile):
+        sys.stderr.write(cyan(f'Error: cannot find Snakefile at {snakefile}\n Check installation\n'))
+        sys.exit(-1)
+    return snakefile
+
+def make_cpg_header(cpg_csv):
+    cpg_string= ["sample"]
+    with open(cpg_csv,"r") as f:
+        cpg_file = csv.DictReader(f)
+        for row in cpg_file:
+            cpg_string.append(row["gene"].lower()+ "_" + row["position(1-based)"])
+    cpg_string = ",".join(cpg_string)
+    return cpg_string
+
+def colour(text, text_colour):
+    bold_text = 'bold' in text_colour
+    text_colour = text_colour.replace('bold', '')
+    underline_text = 'underline' in text_colour
+    text_colour = text_colour.replace('underline', '')
+    text_colour = text_colour.replace('_', '')
+    text_colour = text_colour.replace(' ', '')
+    text_colour = text_colour.lower()
+    if 'red' in text_colour:
+        coloured_text = RED
+    elif 'green' in text_colour:
+        coloured_text = GREEN
+    elif 'yellow' in text_colour:
+        coloured_text = YELLOW
+    elif 'dim' in text_colour:
+        coloured_text = DIM
+    elif 'cyan' in text_colour:
+        coloured_text = 'cyan'
+    else:
+        coloured_text = ''
+    if bold_text:
+        coloured_text += BOLD
+    if underline_text:
+        coloured_text += UNDERLINE
+    if not coloured_text:
+        return text
+    coloured_text += text + END_FORMATTING
+    return coloured_text
+
+def red(text):
+    return RED + text + END_FORMATTING
+
+def cyan(text):
+    return CYAN + text + END_FORMATTING
+
+def green(text):
+    return GREEN + text + END_FORMATTING
+
+def yellow(text):
+    return YELLOW + text + END_FORMATTING
+
+def bold_underline(text):
+    return BOLD + UNDERLINE + text + END_FORMATTING
